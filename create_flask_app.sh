605fdde96b05d94146c767dd47b6df22d71322f5
SCHUTZ Lucas <lucas.schutz0954@gmail.com>
2020-01-22 18:24:06 +0100

    Remove 0 file error, copyright, create a Procfile

--- create_flask_app.sh
+++ create_flask_app.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+#This script create a simple flask application 
+
+# create by : SCHUTZ Lucas
+# http://lucas-schutz.tech
+
 projectName=$1
-projectName="$( echo -e $projectName | tr '-' '_')"
+projectName="$( echo $projectName | tr '-' '_')"
 
 echo 'creating ' $projectName ' app'
 
-if [ $# > 0 ]
+if [ $# -gt 0 ]
 then
     if [ -e $projectName ] && [ -d $projectName ]
     then 
-        echo 'this folder already exist'
+        echo 'Error! : this folder already exist'
     else
         mkdir $projectName
         cd $projectName
         touch setup.py
 
         setup_data='from setuptools import setup\n\nsetup(\n\tname="'$projectName'",\n\tpackages=["'$projectName'"],\n\tinclude_package_data=True,\n\tinstall_requires=[\n\t\t"flask",\n\t\t"flask_sqlalchemy"\n\t],\n)'
-
         echo -e $setup_data >> setup.py
 
+		#make run.sh exacutable 
         touch run.sh
         if [ ! -x $run.sh ]
         then 
@@ -26,9 +31,14 @@ then
         fi
 
         run_script='#!/bin/bash \nBASEDIR=$(dirname "$0")\ncd $BASEDIR\n\nexport FLASK_APP='$projectName'\nexport FLASK_ENV=development\nflask run'
-
         echo -e $run_script >> run.sh
 
+		#Procfile use to push to heroku
+		touch Procfile
+		procfile_data='web: gunicorn '$projectName':app'
+		echo -e $procfile_data >> Procfile
+
+		#folder who will contains source code
         mkdir $projectName
         cd $projectName
 
@@ -37,5 +47,8 @@ then
         init_data='from flask import Flask\nfrom flask_sqlalchemy import SQLAlchemy\n\napp = Flask(__name__)\n\napp.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"\n\ndb = SQLAlchemy(app)\n@app.route("/")\ndef index():\n\treturn "welcome to your new project"'
 
         echo -e $init_data >> __init__.py
+
     fi
+else
+	echo "Error! : Enter your project's name"
 fi
